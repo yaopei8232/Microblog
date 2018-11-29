0a6a1c4311dc4c282e955d9c95fa070d8e8ac2c7
yaopei8232 <1064101435@qq.com>
2018-11-29 17:59:41 +0800

    hello1

---
+++ src/com/Microblog/controller/Hello.java
@@ -0,0 +1,10 @@
+package com.Microblog.controller;
+
+/**
+ * Created by ibf on 2018/11/29.
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
